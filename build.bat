bd4f90838d82e41f1840de5089abf1c5389e0467
Darth Affe <darthaffe@wyrez.org>
2023-12-15 10:21:47 +0100

    Fixed build-script

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ if not exist stable-diffusion.cpp-build (
 )
 
 cd stable-diffusion.cpp-build
+git fetch
 git checkout 4b95d98404bbfe91698fd41b0f514656e358163a
 
 if not exist build (
@@ -19,8 +20,6 @@ cd ..\..
 
 dotnet publish -c Release -o bin
 
-copy .\stable-diffusion.cpp-build\build\bin\Release\sd-abi.dll .\StableDiffusion.NET\bin\Debug\net8.0\sd-abi.dll
-copy .\stable-diffusion.cpp-build\build\bin\Release\sd-abi.dll .\StableDiffusion.NET\bin\Debug\net8.0\sd-abi.dll
-copy .\stable-diffusion.cpp-build\build\bin\Release\sd-abi.dll .\StableDiffusion.NET\bin\Debug\net8.0\sd-abi.dll
+copy .\stable-diffusion.cpp-build\build\bin\Release\sd-abi.dll .\bin\sd-abi.dll
 
 pause
