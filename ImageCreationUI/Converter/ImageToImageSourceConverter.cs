a19812896742a5036979b28d7555a28e76bb6d38
Darth Affe <darthaffe@wyrez.org>
2024-03-22 23:45:41 +0100

    Added example application

---
+++ ImageCreationUI/Converter/ImageToImageSourceConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using StableDiffusion.NET.Helper.Images;
+
+namespace ImageCreationUI.Converter;
+
+[ValueConversion(typeof(IImage), typeof(ImageSource))]
+public class ImageToImageSourceConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        Bitmap? bitmap = (value as IImage)?.ToBitmap();
+        if (bitmap == null) return null;
+
+        using MemoryStream ms = new();
+        bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+        ms.Position = 0;
+
+        BitmapImage bitmapImage = new();
+        bitmapImage.BeginInit();
+        bitmapImage.StreamSource = ms;
+        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+        bitmapImage.EndInit();
+
+        return bitmapImage;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotSupportedException();
+}
---
+++ ImageCreationUI/Extensions/ImageExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using StableDiffusion.NET.Helper.Images.Colors;
+using StableDiffusion.NET.Helper.Images;
+using System.IO;
+
+namespace ImageCreationUI;
+
+public static class ImageExtension
+{
+    public static Bitmap ToBitmap(this IImage image) => image.AsRefImage<ColorRGB>().ToBitmap();
+    public static Bitmap ToBitmap(this Image<ColorRGB> image) => image.AsRefImage<ColorRGB>().ToBitmap();
+
+    public static unsafe Bitmap ToBitmap(this RefImage<ColorRGB> image)
+    {
+        Bitmap output = new(image.Width, image.Height, PixelFormat.Format24bppRgb);
+        Rectangle rect = new(0, 0, image.Width, image.Height);
+        BitmapData bmpData = output.LockBits(rect, ImageLockMode.ReadWrite, output.PixelFormat);
+
+        nint ptr = bmpData.Scan0;
+        foreach (ReadOnlyRefEnumerable<ColorRGB> row in image.Rows)
+        {
+            Span<ColorBGR> target = new((void*)ptr, bmpData.Stride);
+            for (int i = 0; i < row.Length; i++)
+            {
+                ColorRGB srcColor = row[i];
+                target[i] = new ColorBGR(srcColor.B, srcColor.G, srcColor.R);
+            }
+
+            ptr += bmpData.Stride;
+        }
+
+        output.UnlockBits(bmpData);
+        return output;
+    }
+
+    public static byte[] ToPng(this IImage image)
+    {
+        using Bitmap bitmap = image.ToBitmap();
+        using MemoryStream ms = new();
+        bitmap.Save(ms, ImageFormat.Png);
+
+        return ms.ToArray();
+    }
+}
